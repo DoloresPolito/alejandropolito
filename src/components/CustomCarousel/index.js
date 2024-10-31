30dbf1055db1647f967f8bb6a5922b87d926b399
Dolores Polito <dolores.polito@gmail.com>
2024-10-31 15:34:08 -0300

    treatment, contact

--- src/components/CustomCarousel/index.js
+++ src/components/CustomCarousel/index.js
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -9,141 +8,73 @@ import Image from "next/image";
 
 export default function SwiperHero() {
   const slides = [
-    {
-      src1: "clinica/v2.png",
-      orientation: "vertical",
-    },  
-
-    {
-      src1: "clinica/cuadradas/2.png",
-      orientation: "horizontal",
-    },
-
-
-    // {
-    //   src1: "clinica/cuadradas/1.png",
-    //   orientation: "horizontal",
-    // },
-
-
-
-    {
-      src1: "clinica/v4.png",
-      orientation: "vertical",
-    },
-
-    {
-      src1: "clinica/cuadradas/7.png",
-      orientation: "horizontal",
-    },
-
- 
-
-    {
-      src1: "clinica/v3.png",
-      orientation: "vertical",
-    },
-
-
-    // {
-    //   src1: "clinica/cuadradas/6.png",
-    //   orientation: "horizontal",
-    // },
-    // {
-    //   src1: "clinica/v5.png",
-    //   orientation: "vertical",
-    // },
-
-    {
-      src1: "clinica/v1.png",
-      orientation: "vertical",
-    },
-
-
-    {
-      src1: "clinica/cuadradas/8.png",
-      orientation: "horizontal",
-    },
-
-    {
-      src1: "clinica/cuadradas/5.png",
-      orientation: "horizontal",
-    },
-    {
-      src1: "clinica/cuadradas/4.png",
-      orientation: "horizontal",
-    },
-    {
-      src1: "clinica/cuadradas/3.png",
-      orientation: "horizontal",
-    },
-
+    { src1: "clinica/v2.png", orientation: "vertical" },
+    { src1: "clinica/h2.png", orientation: "horizontal" },
+    { src1: "clinica/v4.png", orientation: "vertical" },
+    { src1: "clinica/h3.png", orientation: "horizontal" },
+    { src1: "clinica/v3.png", orientation: "vertical" },
+    { src1: "clinica/v1.png", orientation: "vertical" },
+    { src1: "clinica/cuadradas/8.png", orientation: "horizontal" },
+    { src1: "clinica/cuadradas/5.png", orientation: "horizontal" },
+    { src1: "clinica/cuadradas/4.png", orientation: "horizontal" },
+    { src1: "clinica/cuadradas/3.png", orientation: "horizontal" },
   ];
 
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.carouselcontainer}>
-          <div className={styles.carouselcontent}>
-            <Swiper
-              loop={true}
-              speed={1000}
-              parallax={true}
-              slidesPerView={2.5} // Mostrar 3 imágenes por vista
-              slidesPerGroup={1} // Mover de una imagen a la vez
-              autoplay={{
-                delay: 3000, // Cambiar cada 3 segundos
-                disableOnInteraction: false,
-              }}
-              // pagination={{
-              //   clickable: true,
-              //   bulletClass: "swiper-pagination-bullet",
-              //   bulletActiveClass: "swiper-pagination-bullet-active",
-              // }}
-              modules={[Parallax, Pagination, Autoplay]}
-              className={styles.mySwiper}
-            >
-              {slides.map((slide, index) => (
-                <SwiperSlide key={index}>
-                  <SlideVertical slide={slide} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
+    <div className={styles.container}>
+      <div className={styles.carouselcontainer}>
+        <div className={styles.carouselcontent}>
+          <Swiper
+            loop={true}
+            speed={1000}
+            parallax={true}
+            slidesPerView={3.5}
+            slidesPerGroup={1}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            breakpoints={{
+              900: {
+                slidesPerView: 2,
+              },
+              600: {
+                slidesPerView: 1.5,
+              },
+              400: {
+                slidesPerView: 1,
+              },
+
+              200: {
+                slidesPerView: 1,
+              },
+       
+            }}
+            modules={[Parallax, Pagination, Autoplay]}
+            className={styles.mySwiper}
+          >
+            {slides.map((slide, index) => (
+              <SwiperSlide key={index}>
+                <SlideVertical slide={slide} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
 const SlideVertical = ({ slide }) => {
-  return slide.orientation === "horizontal" ? (
+  return (
     <div className={styles.slide}>
-      <div className={styles.imagecontainerhorizontal}>
-        <Image
-          src={`/images/${slide.src1}`}
-          alt="image"
-          width={600}
-          height={700}
-          priority
-        />
-      </div>
-    </div>
-  ) : slide.orientation === "vertical" ? (
-    <div className={styles.slide}>
-      <div className={styles.imagecontainervertical}>
-        <Image
-          src={`/images/${slide.src1}`}
-          alt="image"
-          width={600}
-          height={700}
-          priority
-        />
-      </div>
-    </div>
-  ) : (
-    <div className={styles.slide}>
-      <div className={styles.imagecontainercuadrada}>
+      <div
+        className={
+          slide.orientation === "horizontal"
+            ? styles.imagecontainerhorizontal
+            : styles.imagecontainervertical
+        }
+      >
         <Image
           src={`/images/${slide.src1}`}
           alt="image"
