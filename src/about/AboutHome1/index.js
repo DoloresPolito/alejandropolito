406f93bcaa3ff95a5fe3f71635f78615433e8b56
Dolores Polito <dolores.polito@gmail.com>
2024-10-31 11:51:06 -0300

    fix animation about?

--- src/about/AboutHome1/index.js
+++ src/about/AboutHome1/index.js
@@ -1,31 +1,45 @@
 "use client"
 
-import styles from "./styles.module.scss";
-import Image from "next/image";
-
+import { useState, useEffect, useRef } from "react";
 import { useScroll, useTransform, motion } from "framer-motion";
-import { useRef } from "react";
+import Image from "next/image";
+import styles from "./styles.module.scss";
 import AnimatedDiv from "@/components/AnimatedDiv";
 import image1 from "../../../public/images/about2.png";
 
 export default function AboutHome1() {
   const container = useRef();
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 750);
+    };
+
+    // Verificar el tamaño de pantalla al cargar
+    handleResize();
+
+    // Escuchar cambios de tamaño de pantalla
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const { scrollYProgress } = useScroll({
     target: container,
     offset: ["start start", "end start"],
-    layoutEffect: false, 
+    layoutEffect: false,
   });
 
-  const y = useTransform(scrollYProgress, [0, 1], ["0vh", "100vh"]);
+  // Menor desplazamiento en pantallas pequeñas
+  const y = useTransform(
+    scrollYProgress,
+    [0, 1],
+    isMobile ? ["0vh", "50vh"] : ["0vh", "100vh"]
+  );
 
   return (
     <div ref={container} className={styles.section}>
-      <motion.div 
-        style={{ 
-          y,     
-        }} 
-        className={styles.motionDiv}
-      >
+      <motion.div style={{ y }} className={styles.motionDiv}>
         <div className={styles.container}>
           <AnimatedDiv>
             <h3>
